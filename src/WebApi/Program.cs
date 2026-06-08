f9a4fbe9fe1f4d231448f16d5de4281500089d9e
Koorosh <kooroshsoleymani98@gmail.com>
2026-06-08 19:37:34 +0330

    Add serilog and mediatR and dependency injection to application

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,18 +1,26 @@
+using Application;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddApplication();
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
